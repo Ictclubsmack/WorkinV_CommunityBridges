f5a9e53434f3ec0b7163848775103e78998bd913
Kimeragb <gabriel@smack.sc.ug>
2025-05-23 00:04:08 +0300

    finalising

--- Ptracking/vite.config.js
+++ Ptracking/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/WorkinV_CommunityBridges/",// 👈 change this to your repo name
 })
